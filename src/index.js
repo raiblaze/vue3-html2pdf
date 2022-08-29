2b8527403fc9061a990c82e480e7b631a694eb6a
ajp-nine <75015704+ajp-nine@users.noreply.github.com>
2022-08-29 18:16:59 +0200

    Remove global installation using Vue.use
    It is no longer supported by Vue 3
    See https://v3-migration.vuejs.org/breaking-changes/global-api.html#a-note-for-plugin-authors for more information

--- src/index.js
+++ src/index.js
@@ -13,20 +13,8 @@ const plugin = {
   install,
 };
 
-// To auto-install when vue is found
-/* global window global */
-let GlobalVue = null;
-if (typeof window !== 'undefined') {
-  GlobalVue = window.Vue;
-} else if (typeof global !== 'undefined') {
-  GlobalVue = global.Vue;
-}
-if (GlobalVue) {
-  GlobalVue.use(plugin);
-}
-
 // Inject install function into component - allows component
-// to be registered via Vue.use() as well as Vue.component()
+// to be registered with Vue.component()
 component.install = install;
 
 // Export component by default
