b554b2d5965f04b9adf1d9cbaf42fa7a6eba42f9
Adrien Lefebvre <adrien@lakaa.io>
2022-09-26 14:25:26 +0200

    fix(Install): remove global installation

--- src/index.js
+++ src/index.js
@@ -8,23 +8,6 @@ function install(Vue) {
   Vue.component('Vue3Html2pdf', component);
 }
 
-// Create module definition for Vue.use()
-const plugin = {
-  install,
-};
-
-// To auto-install when vue is found
-/* global window global */
-let GlobalVue = null;
-if (typeof window !== 'undefined') {
-  GlobalVue = window.Vue;
-} else if (typeof global !== 'undefined') {
-  GlobalVue = global.Vue;
-}
-if (GlobalVue) {
-  GlobalVue.use(plugin);
-}
-
 // Inject install function into component - allows component
 // to be registered via Vue.use() as well as Vue.component()
 component.install = install;
