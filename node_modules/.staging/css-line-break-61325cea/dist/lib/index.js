f5067829b65018c85698cac60afde79153e2548a
shinenbayargansukh <shinenbayar@ailab.mn>
2023-03-29 17:54:41 +0800

    [FIX] jspdf version

--- node_modules/.staging/css-line-break-61325cea/dist/lib/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var Util_1 = require("./Util");
-exports.toCodePoints = Util_1.toCodePoints;
-exports.fromCodePoint = Util_1.fromCodePoint;
-var LineBreak_1 = require("./LineBreak");
-exports.LineBreaker = LineBreak_1.LineBreaker;
-//# sourceMappingURL=index.js.map
